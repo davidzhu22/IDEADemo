8766b2bb07c1211042b910b33da9a51b63cd88cb
davidzhu22 <90352709+davidzhu22@users.noreply.github.com>
2023-04-21 16:25:57 -0700

    Initial commit

---
+++ module04_java_01/src/com/atguigu/java/HelloJava.java
@@ -0,0 +1,16 @@
+package com.atguigu.java;
+
+/**
+ * ClassName: HelloJava
+ * Package: com.atguigu.java
+ * Description:
+ *
+ * @Author: David Zhu
+ * @Create: 4/20/2023 - 11:36 AM
+ * @Version: v1.0
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("This is regular java project");
+    }
+}
---
+++ src/com/atguigu/idea/HelloIDEA.java
@@ -0,0 +1,13 @@
+package com.atguigu.idea;
+
+/**
+ * ClassName: HelloIDEA
+ * Package: com.atguigu.idea
+ * Description:
+ *
+ * @Author: David Zhu
+ * @Create: 4/19/2023 - 11:32 AM
+ * @Version: v1.0
+ */
+public class HelloIDEA {
+}
