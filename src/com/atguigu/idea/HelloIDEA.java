539ba389a20a27ba5854c86a4e1bae85e0e60941
davidzhu22 <90352709+davidzhu22@users.noreply.github.com>
2023-04-21 16:34:26 -0700

    update HelloIDEA class add main and print out one line

--- src/com/atguigu/idea/HelloIDEA.java
+++ src/com/atguigu/idea/HelloIDEA.java
@@ -10,4 +10,8 @@ package com.atguigu.idea;
  * @Version: v1.0
  */
 public class HelloIDEA {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, GitHub, Are you going well?");
+    }
 }
